fef4d267aae5cc1cfce9f4bf9075cc67f77302da
Hoós Roland <shadeeygg@gmail.com>
2022-11-10 14:35:30 +0100

    Commmmmit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const titleRegex = new RegExp('[A-Za-z ,.-]+$');
 let artworks = [];
 document.addEventListener('DOMContentLoaded', init);
 function init() {
+    var _a;
+    (_a = document.getElementById('add')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', felvesz);
+}
+function felvesz() {
+    let title = document.getElementById('title').value;
+    let year = document.getElementById('year').value;
+    if (title.length < 1) {
+        document.getElementById('titleerrorMsg').textContent = "A műtermék címe nem lehet üres!";
+    }
+    else if (!titleRegex.test(title)) {
+        document.getElementById('titleerrorMsg').textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!";
+    }
+    else {
+        document.getElementById('titleerrorMsg').textContent = "";
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Artwork } from "./artwork";
 import { Statue } from "./statue";
 
+const titleRegex = new RegExp('[A-Za-z ,.-]+$')
+
 let artworks: Artwork[] = [];
 
 document.addEventListener('DOMContentLoaded', init)
@@ -11,5 +13,21 @@ function init() {
 
 function felvesz() {
     let title = (document.getElementById('title') as HTMLInputElement).value;
-    console.log(title)
+    let year = (document.getElementById('year') as HTMLInputElement).value;
+
+    if(title.length < 1) {
+        (document.getElementById('titleerrorMsg') as HTMLElement).textContent = "A műtermék címe nem lehet üres!"
+    }
+
+    else if(!titleRegex.test(title)){
+        (document.getElementById('titleerrorMsg') as HTMLElement).textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!"
+    }
+    
+    else {
+        (document.getElementById('titleerrorMsg') as HTMLElement).textContent = ""
+    }
+
+
+
+
 }
