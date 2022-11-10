96c2ccca1443404e222c3ebff1950a8eb0f896bf
Hoós Roland <shadeeygg@gmail.com>
2022-11-10 14:06:30 +0100

    HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let artworks = [];
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Artwork } from "./artwork";
+import { Statue } from "./statue";
+
+let artworks: Artwork[] = [];
+
+document.addEventListener('DOMContentLoaded', init)
+
+function init() {
+    
+}
