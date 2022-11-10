5373f008cdd915117aa52d96fe2ebebe2faf5a59
Hoós Roland <shadeeygg@gmail.com>
2022-11-10 14:16:06 +0100

    Commit

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,10 @@ let artworks: Artwork[] = [];
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-    
+    document.getElementById('add')?.addEventListener('click', felvesz)
+}
+
+function felvesz() {
+    let title = (document.getElementById('title') as HTMLInputElement).value;
+    console.log(title)
 }
