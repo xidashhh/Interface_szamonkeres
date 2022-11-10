7b67b9056eedd07ad402981ba72bc496faa0bc97
Hoós Roland <shadeeygg@gmail.com>
2022-11-10 15:00:46 +0100

    Feladat befejezése

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const statue_1 = require("./statue");
 const titleRegex = new RegExp('[A-Za-z ,.-]+$');
 let artworks = [];
+let amount = 0;
+let priceSum = 0;
 document.addEventListener('DOMContentLoaded', init);
 function init() {
     var _a;
@@ -12,32 +15,58 @@ function felvesz() {
     let year = document.getElementById('year').value;
     let currentYear = new Date().getFullYear();
     let price = document.getElementById('price').value;
-    let magassag = document.getElementById('height').value;
+    let height = document.getElementById('height').value;
+    let validateValue = true;
     if (title.length == 0) {
         document.getElementById('titleErrorMsg').textContent = "A műtermék címe nem lehet üres!";
+        validateValue = false;
     }
     else if (!titleRegex.test(title)) {
         document.getElementById('titleErrorMsg').textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!";
+        validateValue = false;
     }
     else {
         document.getElementById('titleErrorMsg').textContent = "";
     }
     if (parseInt(year) > currentYear) {
         document.getElementById('yearErrorMsg').textContent = "Az év max " + currentYear + " lehet!";
+        validateValue = false;
     }
     else if (year.length == 0) {
         document.getElementById('yearErrorMsg').textContent = "Az év nem lehet üres!";
+        validateValue = false;
     }
     else {
         document.getElementById('yearErrorMsg').textContent = "";
     }
     if (parseInt(price) < 1) {
         document.getElementById('priceErrorMsg').textContent = "Az ár nem lehet kisebb, mint 1!";
+        validateValue = false;
     }
     else if (price.length == 0) {
         document.getElementById('priceErrorMsg').textContent = "Az ár nem lehet üres!";
+        validateValue = false;
     }
     else {
         document.getElementById('priceErrorMsg').textContent = "";
+    }
+    if (parseInt(height) < 20) {
+        document.getElementById('heightErrorMsg').textContent = "A magasság nem lehet kisebb, mint 20!";
+        validateValue = false;
+    }
+    else if (height.length == 0) {
+        document.getElementById('heightErrorMsg').textContent = "A magasság nem lehet üres!";
+        validateValue = false;
+    }
+    else {
+        document.getElementById('heightErrorMsg').textContent = "";
+    }
+    console.log(validateValue);
+    if (validateValue) {
+        artworks.push(new statue_1.Statue(title, parseInt(year), parseInt(price), parseInt(height)));
+        amount++;
+        priceSum += parseInt(price);
+        document.getElementById('amount').textContent = "" + amount;
+        document.getElementById('priceSum').textContent = "" + priceSum;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { Statue } from "./statue";
 const titleRegex = new RegExp('[A-Za-z ,.-]+$')
 
 let artworks: Artwork[] = [];
-
+let amount = 0;
+let priceSum = 0;
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
@@ -13,20 +14,20 @@ function init() {
 
 function felvesz() {
     let title = (document.getElementById('title') as HTMLInputElement).value;
-
     let year = (document.getElementById('year') as HTMLInputElement).value;
     let currentYear = new Date().getFullYear();
-
     let price = (document.getElementById('price') as HTMLInputElement).value;
-
-    let magassag = (document.getElementById('height') as HTMLInputElement).value;
+    let height = (document.getElementById('height') as HTMLInputElement).value;
+    let validateValue = true;
 
     if(title.length == 0) {
         (document.getElementById('titleErrorMsg') as HTMLElement).textContent = "A műtermék címe nem lehet üres!"
+        validateValue = false;
     }
 
     else if(!titleRegex.test(title)){
         (document.getElementById('titleErrorMsg') as HTMLElement).textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!"
+        validateValue = false;
     }
     
     else {
@@ -37,9 +38,11 @@ function felvesz() {
 
     if (parseInt(year) > currentYear){
         (document.getElementById('yearErrorMsg') as HTMLElement).textContent = "Az év max " + currentYear + " lehet!"
+        validateValue = false;
     }
     else if (year.length == 0){
         (document.getElementById('yearErrorMsg') as HTMLElement).textContent = "Az év nem lehet üres!"
+        validateValue = false;
     }
     else {
         (document.getElementById('yearErrorMsg') as HTMLElement).textContent = ""
@@ -48,12 +51,36 @@ function felvesz() {
 
     if (parseInt(price) < 1){
         (document.getElementById('priceErrorMsg') as HTMLElement).textContent = "Az ár nem lehet kisebb, mint 1!"
+        validateValue = false;
     }
     else if (price.length == 0){
         (document.getElementById('priceErrorMsg') as HTMLElement).textContent = "Az ár nem lehet üres!"
+        validateValue = false;
     }
     else {
         (document.getElementById('priceErrorMsg') as HTMLElement).textContent = ""
+    }
+
+    if (parseInt(height) < 20){
+        (document.getElementById('heightErrorMsg') as HTMLElement).textContent = "A magasság nem lehet kisebb, mint 20!"
+        validateValue = false;
+    }
+    else if (height.length == 0){
+        (document.getElementById('heightErrorMsg') as HTMLElement).textContent = "A magasság nem lehet üres!"
+        validateValue = false;
+    }
+    else {
+        (document.getElementById('heightErrorMsg') as HTMLElement).textContent = ""
+    }
+
+    console.log(validateValue);
+
+    if(validateValue){
+        artworks.push(new Statue(title, parseInt(year), parseInt(price), parseInt(height)))
+        amount++;
+        priceSum += parseInt(price);
+        (document.getElementById('amount') as HTMLElement).textContent = "" + amount;
+        (document.getElementById('priceSum') as HTMLElement).textContent = "" + priceSum;
     }
 
 
