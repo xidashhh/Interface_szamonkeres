0a91387d49c8902468e24af9cf3d5dad577abd71
Hoós Roland <shadeeygg@gmail.com>
2022-11-10 14:43:07 +0100

    Commmmmit

--- src/index.js
+++ src/index.js
@@ -10,13 +10,23 @@ function init() {
 function felvesz() {
     let title = document.getElementById('title').value;
     let year = document.getElementById('year').value;
-    if (title.length < 1) {
-        document.getElementById('titleerrorMsg').textContent = "A műtermék címe nem lehet üres!";
+    let currentYear = new Date().getFullYear();
+    if (title.length == 0) {
+        document.getElementById('titleErrorMsg').textContent = "A műtermék címe nem lehet üres!";
     }
     else if (!titleRegex.test(title)) {
-        document.getElementById('titleerrorMsg').textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!";
+        document.getElementById('titleErrorMsg').textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!";
     }
     else {
-        document.getElementById('titleerrorMsg').textContent = "";
+        document.getElementById('titleErrorMsg').textContent = "";
+    }
+    if (parseInt(year) > currentYear) {
+        document.getElementById('yearErrorMsg').textContent = "Az év max " + currentYear + " lehet!";
+    }
+    else if (year.length == 0) {
+        document.getElementById('yearErrorMsg').textContent = "Az év nem lehet üres!";
+    }
+    else {
+        document.getElementById('yearErrorMsg').textContent = "";
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -14,18 +14,32 @@ function init() {
 function felvesz() {
     let title = (document.getElementById('title') as HTMLInputElement).value;
     let year = (document.getElementById('year') as HTMLInputElement).value;
+    let currentYear = new Date().getFullYear();
 
-    if(title.length < 1) {
-        (document.getElementById('titleerrorMsg') as HTMLElement).textContent = "A műtermék címe nem lehet üres!"
+    if(title.length == 0) {
+        (document.getElementById('titleErrorMsg') as HTMLElement).textContent = "A műtermék címe nem lehet üres!"
     }
 
     else if(!titleRegex.test(title)){
-        (document.getElementById('titleerrorMsg') as HTMLElement).textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!"
+        (document.getElementById('titleErrorMsg') as HTMLElement).textContent = "A műtermék csak az angol ABC betűit tartalmazhatja, illetve pontot, vesszőt, kötőjelet és szóközt!"
     }
     
     else {
-        (document.getElementById('titleerrorMsg') as HTMLElement).textContent = ""
+        (document.getElementById('titleErrorMsg') as HTMLElement).textContent = ""
     }
+
+    if (parseInt(year) > currentYear){
+        (document.getElementById('yearErrorMsg') as HTMLElement).textContent = "Az év max " + currentYear + " lehet!"
+    }
+    else if (year.length == 0){
+        (document.getElementById('yearErrorMsg') as HTMLElement).textContent = "Az év nem lehet üres!"
+
+    }
+    else {
+        (document.getElementById('yearErrorMsg') as HTMLElement).textContent = ""
+
+    }
+
 
 
 
